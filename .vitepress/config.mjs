c7a7e590e35aa547d5f997e60039f771e604001a
Boreas618 <21307130094@m.fudan.edu.cn>
2023-10-18 02:34:01 +0800

    initial commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Yi Sun's Blog",
+  description: "This is Yi Sun's blog.",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Me', link: '/' },
+      { text: 'Posts', link: '/posts/monitor' }
+    ],
+    sidebar: {
+      '/': {
+        text: 'Me',
+        items: [
+        ],
+        collapsed: true,
+      },
+      '/posts/': {
+        text: 'System',
+        items: [
+          { text: 'Monitor, Condition Variables and Three Semantics', link: '/posts/monitor' },
+        ]
+      }
+    },
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
---
+++ index.md
@@ -0,0 +1,10 @@
+---
+layout: doc
+sidebar: false
+aside: false
+outline: false
+---
+
+# Yi Sun
+
+My name is Yi Sun and I am currently a second-year student studying Computer Science at Fudan University. At the moment, I am particularly interested in topics such as operating systems, deep learning, natural language processing, and visualization.
